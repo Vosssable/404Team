a9d2619d022a4ac0932c2f527f1033e6e1b28d5c
maksguido <maksguido@gmail.com>
2025-05-31 13:28:47 +0300

    feat: #50781972 уменьшена версия роутера

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,18 +1,19 @@
 import { RouterProvider } from 'react-router-dom'
 import { router } from './app/router'
 import './App.css'
+import { useEffect } from 'react'
 
 function App() {
-  // useEffect(() => {
-  //   const fetchServerData = async () => {
-  //     const url = `http://localhost:${__SERVER_PORT__}`
-  //     const response = await fetch(url)
-  //     const data = await response.json()
-  //     console.log(data)
-  //   }
+  useEffect(() => {
+    const fetchServerData = async () => {
+      const url = `http://localhost:${__SERVER_PORT__}`
+      const response = await fetch(url)
+      const data = await response.json()
+      console.log(data)
+    }
 
-  //   fetchServerData()
-  // }, [])
+    fetchServerData()
+  }, [])
   return <RouterProvider router={router} />
 }
 
