5c0a6b327b16683a7e116dd436460a529dc64664
maksguido <maksguido@gmail.com>
2025-06-14 20:48:50 +0300

    настройка тестов для стилей

--- packages/client/src/pages/error-pages/ErrorPage.tsx
+++ packages/client/src/pages/error-pages/ErrorPage.tsx
@@ -1,5 +1,5 @@
 import { useCallback } from 'react'
-import styles from './ErrorPage.module.css'
+import './ErrorPage.css'
 
 const ErrorPage = () => {
   const handleReload = useCallback(() => {
@@ -7,13 +7,13 @@ const ErrorPage = () => {
   }, [])
 
   return (
-    <div className={`${styles.errorPage} ${styles.error500}`}>
-      <header className={styles.header}>
-        <button className={styles.link} onClick={handleReload}>
+    <div className="errorPage error500">
+      <header className="header">
+        <button className="link" onClick={handleReload}>
           Перезагрузить страницу
         </button>
       </header>
-      <div className={styles.spacer} />
+      <div className="spacer" />
     </div>
   )
 }
--- packages/client/src/pages/error-pages/NotFoundPage.tsx
+++ packages/client/src/pages/error-pages/NotFoundPage.tsx
@@ -1,19 +1,19 @@
 import { Link, useNavigate } from 'react-router-dom'
-import styles from './ErrorPage.module.css'
+import './ErrorPage.css'
 
 const NotFoundPage = () => {
   const navigate = useNavigate()
   return (
-    <div className={`${styles.errorPage} ${styles.error404}`}>
-      <header className={styles.header}>
-        <Link to="/" className={styles.link}>
+    <div className="errorPage error404">
+      <header className="header">
+        <Link to="/" className="link">
           На главную
         </Link>
-        <button className={styles.link} onClick={() => navigate(-1)}>
+        <button className="link" onClick={() => navigate(-1)}>
           Назад
         </button>
       </header>
-      <div className={styles.spacer} />
+      <div className="spacer" />
     </div>
   )
 }
