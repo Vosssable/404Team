01a6a98440531b083e2382b614ca59a0a4f2c429
Aleksei Erofeev <54234310+Aleks3y1@users.noreply.github.com>
2025-06-15 22:07:59 +0500

    -Добавляет страницу регистрации и авторизации; -Добавляет переменные в стили;

--- packages/client/src/pages/LoginPage.tsx
+++ packages/client/src/pages/LoginPage.tsx
@@ -1,7 +1,21 @@
+import './RegisterPage.css'
+import FormToFill from '../components/FormToFill'
+
+const props = {
+  description: 'Вход',
+  inputs: [
+    { type: 'text', id: 'login', label: 'Логин', name: 'login' },
+    { type: 'password', id: 'password', label: 'Пароль', name: 'password' },
+  ],
+  buttonText: 'Войти',
+  href: '/register',
+  linkText: 'Еще не зарегистрированы?',
+}
+
 const LoginPage = () => {
   return (
-    <div>
-      <h1>Страница входа</h1>
+    <div className="container-fluid bg__auth vw-100 vh-100 p-0 m-0 d-flex justify-content-center align-items-center">
+      <FormToFill {...props} />
     </div>
   )
 }
