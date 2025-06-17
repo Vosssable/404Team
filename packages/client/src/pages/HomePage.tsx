8c895e5f38fc853039998b575a482fd5754133e7
yakovalt2 <yakovalt2@gmail.com>
2025-06-17 20:25:56 +0300

    Вёрстка главной страницы

--- packages/client/src/pages/HomePage.tsx
+++ packages/client/src/pages/HomePage.tsx
@@ -1,9 +1,28 @@
-const HomePage = () => {
+import { Link } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './HomePage.css'
+
+export default function HomePage() {
   return (
-    <div>
-      <h1>Главная страница</h1>
+    <div className="homepage-background d-flex flex-column justify-content-center align-items-center text-center">
+      <img
+        src="/img/logo.png"
+        alt="Волк ловит яйца"
+        className="mb-4"
+        style={{ maxWidth: '400px' }}
+      />
+      <h1 className="h3 fw-bold text-light mb-4">Волк ловит яйца</h1>
+      <p className="lead text-light mb-5">
+        Помоги волку поймать все яйца и набери рекорд!
+      </p>
+      <div className="d-flex gap-3">
+        <Link to="/register" className="btn btn-success btn-lg startgame-btn">
+          Регистрация
+        </Link>
+        <Link to="/forum" className="btn btn-outline-light btn-lg">
+          Форум
+        </Link>
+      </div>
     </div>
   )
 }
-
-export default HomePage
