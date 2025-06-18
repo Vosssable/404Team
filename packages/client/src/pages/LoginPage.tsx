52525ff4c9633e96b7e9b07f94fea37dfa6f4d68
Aleksei Erofeev <54234310+Aleks3y1@users.noreply.github.com>
2025-06-18 21:18:45 +0500

    -Исправляет комментарии по реквесту. -Добавляет файл с пропсами; -Переименовывает картинки.

--- packages/client/src/pages/LoginPage.tsx
+++ packages/client/src/pages/LoginPage.tsx
@@ -1,21 +1,11 @@
 import './RegisterPage.css'
 import FormToFill from '../components/FormToFill'
-
-const props = {
-  description: 'Вход',
-  inputs: [
-    { type: 'text', id: 'login', label: 'Логин', name: 'login' },
-    { type: 'password', id: 'password', label: 'Пароль', name: 'password' },
-  ],
-  buttonText: 'Войти',
-  href: '/register',
-  linkText: 'Еще не зарегистрированы?',
-}
+import { loginProps } from '../types/otherProps'
 
 const LoginPage = () => {
   return (
     <div className="container-fluid bg__auth vw-100 vh-100 p-0 m-0 d-flex justify-content-center align-items-center">
-      <FormToFill {...props} />
+      <FormToFill {...loginProps} />
     </div>
   )
 }
--- packages/client/src/pages/RegisterPage.tsx
+++ packages/client/src/pages/RegisterPage.tsx
@@ -1,31 +1,11 @@
 import './RegisterPage.css'
 import FormToFill from '../components/FormToFill'
-
-const props = {
-  description: 'Регистрация',
-  inputs: [
-    { type: 'text', id: 'name', label: 'Имя', name: 'first_name' },
-    { type: 'text', id: 'firstName', label: 'Фамилия', name: 'second_name' },
-    { type: 'text', id: 'login', label: 'Логин', name: 'login' },
-    { type: 'tel', id: 'tel', label: 'Номер телефона', name: 'phone' },
-    { type: 'email', id: 'email', label: 'Почта', name: 'email' },
-    { type: 'password', id: 'password', label: 'Пароль', name: 'password' },
-    {
-      type: 'password',
-      id: 'retryPassword',
-      label: 'Повторите пароль',
-      name: 'retryPassword',
-    },
-  ],
-  buttonText: 'Зарегистрироваться',
-  href: '/login',
-  linkText: 'Войти',
-}
+import { registerProps } from '../types/otherProps'
 
 const RegisterPage = () => {
   return (
     <div className="container-fluid bg__auth vw-100 vh-100 p-0 m-0 d-flex justify-content-center align-items-center">
-      <FormToFill {...props} />
+      <FormToFill {...registerProps} />
     </div>
   )
 }
