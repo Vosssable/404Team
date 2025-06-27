0fcbffe2fd43b1c35dca29091eb4a1881c78e11b
Aleksei Erofeev <54234310+Aleks3y1@users.noreply.github.com>
2025-06-27 23:40:33 +0500

    -Исправляет в соответствии с комментариями.

--- packages/client/src/hooks/ValidationHook.tsx
+++ packages/client/src/hooks/ValidationHook.tsx
@@ -2,26 +2,22 @@ import { Validation } from './Validation'
 
 export function validationHook(form: HTMLFormElement): boolean {
   let isValid = true
+
   form.querySelectorAll('input').forEach(el => {
     const input = el as HTMLInputElement
-
-    if (input.name === 'retryPassword') {
-      const errEl = input.nextElementSibling as HTMLElement
-      input.classList.remove('input-error')
-      if (errEl) errEl.textContent = ''
-    }
 
     if (input.name === 'retryPassword') {
       const pwdInput = form.querySelector(
         "input[name='password']"
       ) as HTMLInputElement
       const pwd = pwdInput ? pwdInput.value : ''
+
       if (input.value !== pwd) {
         isValid = false
-        input.classList.add('input-error')
-        const err = input.nextElementSibling as HTMLElement
-        if (err) err.textContent = 'Пароли не совпадают'
+        Validation.setError(input, 'Пароли не совпадают')
         return
+      } else {
+        Validation.setError(input, null)
       }
     }
 
@@ -29,5 +25,6 @@ export function validationHook(form: HTMLFormElement): boolean {
       isValid = false
     }
   })
+
   return isValid
 }
