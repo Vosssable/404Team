b98af4ddf011e6831f473a06f2a5277c874dd17f
yakovalt2 <yakovalt2@gmail.com>
2025-06-29 12:57:51 +0200

    Добавлен gameSlice для хранения состояния игры

--- packages/client/src/store/rootReducer.tsx
+++ packages/client/src/store/rootReducer.tsx
@@ -1,6 +1,8 @@
 import { combineReducers } from '@reduxjs/toolkit'
 import userReducer from './userSlice'
+import gameReducer from './gameSlice'
 
 export default combineReducers({
   user: userReducer,
+  game: gameReducer,
 })
