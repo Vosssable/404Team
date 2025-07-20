4458cb8570b6f523c92cf80287fc498be779e615
Aleksei Erofeev <54234310+Aleks3y1@users.noreply.github.com>
2025-07-20 17:29:38 +0300

    Добавляет SSR в проект, без роутинга и редакса.

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,12 +1,14 @@
-import { RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { router } from './app/router'
 import './App.css'
 import { useEffect } from 'react'
 
+const routers = createBrowserRouter(router)
+
 function App() {
   useEffect(() => {
     const fetchServerData = async () => {
-      const url = `http://localhost:${__SERVER_PORT__}`
+      const url = `http://localhost:3000`
       const response = await fetch(url)
       const data = await response.json()
       console.log(data)
@@ -14,7 +16,7 @@ function App() {
 
     fetchServerData()
   }, [])
-  return <RouterProvider router={router} />
+  return <RouterProvider router={routers} />
 }
 
 export default App
--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -7,7 +7,8 @@ import ErrorBoundary from './pages/error-pages/ErrorBoundary'
 import { Provider } from 'react-redux'
 import store from './store'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.hydrateRoot(
+  document.getElementById('root') as HTMLElement,
   <React.StrictMode>
     <Provider store={store}>
       <ErrorBoundary>
