ae91da59a4ecb3c658470266d90f5d88127aa3ec
artemka <rambler601@yandex.ru>
2025-08-03 01:02:53 +0300

    [ПРОВЕРЯЛКИ] - В этом коммите все, чтобы проверять серверную часть

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -9,7 +9,9 @@ function App() {
 }
 
 // для тестирования
-window.helpers = {
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+window['helpers'] = {
   getEmoji: async () => {
     const test = await fetch('http://localhost:3001/emojis', { method: 'GET' })
     test.json().then(json => {
@@ -19,6 +21,37 @@ window.helpers = {
   getEmoj: async (id: number) => {
     const test = await fetch(`http://localhost:3001/emojis/${id}`, {
       method: 'GET',
+    })
+    test.json().then(json => {
+      console.log(json)
+    })
+  },
+  getUser: async (username: string) => {
+    const test = await fetch(`http://localhost:3001/user/${username}`, {
+      method: 'GET',
+    })
+    test.json().then(json => {
+      console.log(json)
+    })
+  },
+  postUser: async (username: string, darkTheme: boolean) => {
+    const test = await fetch(`http://localhost:3001/user`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        username: username,
+        darkTheme: darkTheme,
+      }),
+    })
+    test.json().then(json => {
+      console.log(json)
+    })
+  },
+  patchUser: async (username: string) => {
+    const test = await fetch(`http://localhost:3001/user/${username}`, {
+      method: 'PATCH',
     })
     test.json().then(json => {
       console.log(json)
